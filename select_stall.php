01a490f12588ede12f3b7accc449ced7a5350dce
pradeep-veera <pradeep.veera@rocketmail.com>
2016-03-21 15:22:17 +0100

    Create select_stall.php

---
+++ select_stall.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous"/>
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous"><!-- Latest compiled and minified JavaScript -->
+    <link rel="stylesheet" href="styles.css"/>
+	<script src="http://code.jquery.com/jquery-2.2.2.min.js"></script>	
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <body class = "Page3">
+		<div class = "container-fluid">
+			<div class = "HeaderPage2 row">
+				<H3 style = "text-align:center">HEADER</h3> 
+			</div>
+			<div class = "Register">
+				
+			</div>
+		</div>	
+	</body>	
+</html>	
+			
